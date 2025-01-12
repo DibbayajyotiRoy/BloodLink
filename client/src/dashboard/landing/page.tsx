5d8852da65e9095961890798c4ca6e0f32e386ff
Nirupam Pal <nirupampal14@gmail.com>
2025-01-12 18:27:56 +0530

    Added navbar to the searching page

--- client/src/dashboard/landing/page.tsx
+++ client/src/dashboard/landing/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Link } from "react-router-dom";
+import axios from "axios";
+// import { Link } from "react-router-dom";
 
 
 
