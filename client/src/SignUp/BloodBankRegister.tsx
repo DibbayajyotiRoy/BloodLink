523dcbca52c2949723cbba2e995e1c213a44f556
Nabhanil <nabhanilchakraborty084@gmail.com>
2025-01-13 16:31:35 +0530

    a litte adjustment

--- client/src/SignUp/BloodBankRegister.tsx
+++ client/src/SignUp/BloodBankRegister.tsx
@@ -7,7 +7,7 @@ import { toast } from "sonner"; // Assuming you use 'sonner' for notifications
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
-  Form,
+  
   FormControl,
   FormDescription,
   FormField,
